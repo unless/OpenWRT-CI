34faf22d7cdccec6f2416cc313696b0731a1f817
unless <jbpv@163.com>
2026-08-01 11:09:37 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -4,42 +4,45 @@
 
 #安装和更新软件包
 UPDATE_PACKAGE() {
-	local PKG_NAME=$1
-	local PKG_REPO=$2
-	local PKG_BRANCH=$3
-	local PKG_SPECIAL=$4
-	local PKG_LIST=("$PKG_NAME" $5)  # 第5个参数为自定义名称列表
-	local REPO_NAME=${PKG_REPO#*/}
+    local PKG_NAME=$1
+    local PKG_REPO=$2
+    local PKG_BRANCH=$3
+    local PKG_SPECIAL=$4
+    local PKG_LIST=($5)   # 第5个参数为自定义名称列表
+    local REPO_NAME=${PKG_REPO#*/}
 
-	echo " "
+    # 如果没有传第5个参数，就用第1个参数
+    if [ -z "$5" ]; then
+        PKG_LIST=("$PKG_NAME")
+    fi
 
 	# 删除本地可能存在的不同名称的软件包
-	for NAME in "${PKG_LIST[@]}"; do
-		# 查找匹配的目录
-		echo "Search directory: $NAME"
-		local FOUND_DIRS=$(find ../feeds/luci/ ../feeds/packages/ -maxdepth 3 -type d -iname "*$NAME" 2>/dev/null)
+    for NAME in "${PKG_LIST[@]}"; do
+        echo "Search directory: $NAME"
+        local FOUND_DIRS=$(find ../feeds/luci/ ../feeds/packages/ -maxdepth 3 -type d -name "$NAME" 2>/dev/null)
 
-		# 删除找到的目录
-		if [ -n "$FOUND_DIRS" ]; then
-			while read -r DIR; do
-				rm -rf "$DIR"
-				echo "Delete directory: $DIR"
-			done <<< "$FOUND_DIRS"
-		else
-			echo "Not fonud directory: $NAME"
-		fi
-	done
+        if [ -n "$FOUND_DIRS" ]; then
+            while read -r DIR; do
+                rm -rf "$DIR"
+                echo "Delete directory: $DIR"
+            done <<< "$FOUND_DIRS"
+        else
+            echo "Not found directory: $NAME"
+        fi
+    done
 
-	# 克隆 GitHub 仓库
-	git clone --depth=1 --single-branch --branch $PKG_BRANCH "https://github.com/$PKG_REPO.git"
+    # 克隆 GitHub 仓库
+    git clone --depth=1 --single-branch --branch "$PKG_BRANCH" "https://github.com/$PKG_REPO.git"
 
-	# 处理克隆的仓库
-	if [[ "$PKG_SPECIAL" == "pkg" ]]; then
-		find ./$REPO_NAME/*/ -maxdepth 3 -type d -iname "*$PKG_NAME*" -prune -exec cp -rf {} ./ \;
-		rm -rf ./$REPO_NAME/
-	elif [[ "$PKG_SPECIAL" == "name" ]]; then
-		mv -f $REPO_NAME $PKG_NAME
-	fi
+    # 处理克隆的仓库
+    if [[ "$PKG_SPECIAL" == "pkg" ]]; then
+        for NAME in ${PKG_LIST[@]}; do
+	    find "./$REPO_NAME" -maxdepth 3 -type d -name "$NAME" -exec echo "Copy package: {}" \; -exec cp -rf {} ./ \;
+        done
+        rm -rf "$REPO_NAME/"
+    elif [[ "$PKG_SPECIAL" == "name" ]]; then
+        mv -f "$REPO_NAME" "$PKG_NAME"
+    fi
 }
 
 # 调用示例
