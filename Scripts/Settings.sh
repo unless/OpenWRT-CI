31e9528c49a966962099b438bf2a2b088c33c812
unless <jbpv@163.com>
2026-08-07 08:56:48 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -89,8 +89,9 @@ if grep -qE '^CONFIG_TARGET_.*_DEVICE_.*040g.*=y' .config; then
 	elif  [[ "${WRT_CONFIG,,}" == *"438"* ]]; then
 #        curl -L https://github.com/unless/immortalwrt/commit/ca7137486af261344e8ae99c73d2451aa18467f6.patch | patch -p1 # cpufreq
 		curl -L https://raw.githubusercontent.com/unless/OpenWRT-CI/main/Scripts/add-wan.patch | patch -p1 # addwan
-		curl -L https://github.com/openwrt/openwrt/pull/24265.patch | patch -p1 # cpufreq
 		curl -L https://raw.githubusercontent.com/unless/OpenWRT-CI/main/Scripts/add-438mb-dts.patch | patch -p1 #438mb
+		curl -L https://github.com/openwrt/openwrt/pull/24265.patch | patch -p1 # cpufreq
+		curl -L https://github.com/openwrt/openwrt/pull/24571.patch | patch -p1 # npu
 		echo "WRT_WIFI=438MB" >> $GITHUB_ENV
 	fi
 fi
